c88ebd7ffbe181b86928d706dc79bcf2dc5b2f6d
Baradun <fdhbkkjd@mail.ru>
2020-10-13 12:42:26 +0800

    First changes

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import sqrt, sin, exp
+from math import sqrt, sin
 a = 123
 H0 = np.array([
     [0., 0.],
@@ -9,20 +9,29 @@ c = sqrt(1-sin(0.437)**2)
 W = np.array([
     [c, 1.],
     [1., c]]) 
-#v = 0
-#H0 = 0
-#H = H0 + v*W
 
+
+def get_A(step, t, A=None):
+    if A is None:
+        v0 = 93536.7
+        n = 10.3
+        v = v0 * np.exp(-1*n*t) #??
+        A = H0 + v*W
+        return A
+    
+    tn = step*t 
+    A = np.exp(t*A*(tn+t/2))
+    return A
 
 t=0.01
-tn = 1 #????
-A = 93536.7
 y = np.arange(0, 1, t)
 F0 = np.array([1.0, 0.0])
+
 for i, t in enumerate(y):
     if i == 0:
-        F_f = F0
+        A = get_A(i,t)
     else:
-        F_f = exp(t*A*(tn+t/2)) * F_f
-print(F_f)
+        A = H0 + get_A(i,t,A)* W
+
+print(A)
 
