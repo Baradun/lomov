da841c12c52a57b1acece9dc6899f13ecafc4dde
Baradun <fdhbkjd@mail.ru>
2021-03-09 17:15:01 +0800

    methods.py: complete tui

--- scripts/methods.py
+++ scripts/methods.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
+import curses
+from copy import deepcopy
 import json
 import os
-import sys
-from multiprocessing import Pool
-from pathlib import Path
 import subprocess
-from subprocess import Popen
+import sys
 import time
+from multiprocessing import Array, Pool, Lock
+from pathlib import Path
+from subprocess import Popen
 
 BASE_DIR = os.getenv('BASE_DIR', 'main')
-RUN_FILE = os.getenv('RUN_FILE', "main")
+RUN_FILE = os.getenv('RUN_FILE', 'main')
 RUNS = int(os.getenv('RUNS', '10'))
 CORES = int(os.getenv('CORES', '12'))
 DATA_DIR = os.getenv('DATA_DIR', 'data')
@@ -18,30 +20,94 @@ JSON_DIR = os.getenv('JSON_DIR', 'methods')
 JSON_WORK = os.getenv('JSON_WORK', 'work.json')
 SCRIPT_DIR = os.getenv('MESON_SOURCE_ROOT', '.')
 WDIR = os.getenv('MESON_BUILD_ROOT', '.')
+METHODS = ['M2', 'M4', 'M6', 'CF4', 'CF4:3']
+
+lock = Lock()
+
+
+class Win:
+    def __init__(self, list_params):
+        self.list_params = deepcopy(list_params)
+        self.screen = curses.initscr()
+
+        self.num_rows, self.num_cols = self.screen.getmaxyx()
+        self.win = curses.newwin(self.num_rows, self.num_cols, 0, 0)
+        self.win.refresh()
+
+        self.values = self.count()
+
+    def count(self):
+        values = []
+        for method in METHODS:
+            cnt = 0
+            for i in self.list_params:
+                if method == i.get('method'):
+                    cnt += 1
+            values.append({'method': method, 'count': cnt})
+        return values
+
+    def count_multi(self):
+        values = []
+        for method in METHODS:
+            cnt = 0
+            for i in self.list_params:
+                if method == i.get('method') and end_process[win.list_params.index(i)] == 0:
+                    cnt += 1
+            values.append({'method': method, 'count': cnt})
+        return values
+
+    @staticmethod
+    def progress(number, totaly, colums_number=60):
+        range = int((colums_number / float(totaly)) * number)
+        return range*'#' + ' '*int(colums_number - range)
+
+    def update(self):
+        new_num_rows, new_num_cols = self.screen.getmaxyx()
+        if new_num_cols != self.num_cols or new_num_rows != self.num_rows:
+            self.num_rows = new_num_rows
+            self.num_cols = new_num_cols
+            self.win = curses.newwin(self.num_rows, self.num_cols, 0, 0)
+
+        values = self.count_multi()
+
+        i = 0
+        for deflot in self.values:
+            for modified in values:
+                if deflot.get('method') == modified.get('method'):
+                    self.win.addstr(i, 0, deflot.get('method'))
+                    total = deflot.get('count')
+                    number = modified.get('count')
+
+                    persent_string = self.progress(number, total)
+
+                    self.win.addstr(
+                        i, 8, f'[{persent_string}] ({number:>5}/{total:>5})')
+                    self.win.refresh()
+
+                    i += 1
+
+    def __del__(self):
+        curses.endwin()
 
 
 def run_subprocess(params):
     """Run a program with given parameters.
 
     """
-    # params = (start, stop, step, method, output_file)
-    command = [ str(params["program"]), params["start"], params["end"],
-            params["step"], params["method"] ]
+    command = [str(params['program']), params['start'], params['end'],
+               params['step'], params['method']]
+
+    
+    # Terminal info
+    end_process[win.list_params.index(params)] = 0
+    lock.acquire()
+    win.update()
+    lock.release()
+
 
     with Popen(command, stdout=subprocess.PIPE, text=True) as proc:
-        with open(params["dat_file"], "w") as f:
+        with open(params['dat_file'], 'w') as f:
             f.write(proc.stdout.read())
-
-    #  try:
-    #      result = subprocess.run(command, capture_output=True, check=True,
-    #              text=True)
-    #      with open(params["dat_file"], "w") as f:
-    #          f.write(result.stdout)
-    #      if result.returncode == 0:
-    #          return 0
-    #      return RuntimeError
-    #  except subprocess.SubprocessError as exeption:
-    #      return exeption
 
 
 def get_params(params_file, data_dir):
@@ -65,15 +131,15 @@ def get_params(params_file, data_dir):
         exe = Path(BASE_DIR) / Path(RUN_FILE)
 
         for j in range(RUNS):
-            dat_file_p = f"{method_name}_{start},{stop}_{step}_r{j}.dat"
+            dat_file_p = f'{method_name}_{start},{stop}_{step}_r{j}.dat'
             dat_file = Path(data_dir / dat_file_p)
             list_params.append({
-                "program" : exe,
-                "start" : start,
-                "end" : stop,
-                "step" : step,
-                "method" : method_name,
-                "dat_file" : dat_file
+                'program': exe,
+                'start': start,
+                'end': stop,
+                'step': step,
+                'method': method_name,
+                'dat_file': dat_file
             })
 
     return list_params
@@ -84,6 +150,16 @@ def run(data_dir, json_file):
 
     """
     list_params = get_params(json_file, data_dir)
+
+    # Create window in terminal 
+    global win
+    global end_process
+    end_process = Array('i', len(list_params))
+    for i in range(len(list_params)):
+        end_process[i] = 1
+    win = Win(list_params)
+
+
     try:
         process_pool = Pool(CORES)
         result = process_pool.map(run_subprocess, list_params)
@@ -102,45 +178,28 @@ if __name__ == '__main__':
         try:
             os.mkdir(data_dir)
         except FileExistsError as err:
-            print(f"We need directory '{DATA_DIR}' to save generated data but \
-                    we got error while trying to create one: {err}")
+            print(f"We need directory '{DATA_DIR}' to save generated data but" +
+                  f'we got error while trying to create one: {err}')
         except:
-            print("Unexpected error: ", sys.exc_info()[0])
+            print('Unexpected error: ', sys.exc_info()[0])
             raise
 
     json_dir = Path(JSON_DIR)
     if not os.path.isdir(json_dir):
-        print(f"We expect that JSON file with work load should be in \
-                '{JSON_DIR}' directory but that one doesn't exist or is not a \
-                directory!")
+        print('We expect that JSON file with work load should be in '
+              f"'{JSON_DIR}' directory but that one doesn't exist or is not a " +
+              'directory!')
         sys.exit(1)
 
-    start_time = time.time()
-    print('start prgrm')
-
-    # LOG_FILE_DIR = 'logs/logs_0.1_0.15/'
-    # PARAMS_FILE = 'logs/methods_0.1_0.15.json'
-    # run('logs/logs_0.1_0.15/', 'logs/methods_0.1_0.15.json')
-
-    # print('#'*80)
-    # print('time = ', time.time() - start_time)
-    # print('#'*80)
-
-    # LOG_FILE_DIR = 'logs/logs_0.1_0.2/'
-    # PARAMS_FILE = 'logs/methods_0.1_0.2.json'
-    # run('logs/logs_0.1_0.2/', 'logs/methods_0.1_0.2.json')
-
-    # print('#'*80)
-    # print('time = ', time.time() - start_time)
-    # print('#'*80)
-
-    #  LOG_FILE_DIR = 'logs_0.1_0.3/'
-    #  PARAMS_FILE = 'methods_0.1_0.3.json'
     json_file = json_dir / JSON_WORK
     if not os.path.isfile(json_file):
         print(f"Can't do anything, we need a '{JSON_WORK}' in '{JSON_DIR}'!")
         sys.exit(2)
+
+    start_time = time.time()
+    print('start prgrm')
+
     run(data_dir, json_file)
 
-    print("end prgrm")
+    print('end prgrm')
     print(time.time() - start_time)
