27ebe8c2e63de36a18d2ff8a0bd9d472a4a47686
Алекс <alex-mb@MacBook-Air-Alex.local>
2021-05-22 16:48:35 +0800

    update all_stat.py

--- scripts/all_stat.py
+++ scripts/all_stat.py
@@ -42,9 +42,40 @@ def gen_gp_dat(data, graf_type=0):
 
     rngs = pd.unique(data['range'])
     hosts = pd.unique(data['host'])
+    steps = pd.unique(data['step'])
 
     # x = steps; y = err
     if graf_type == 0:
+        data_t = data.copy()
+        data_t.insert(8, 're', 0)
+        data_t.insert(9, 'sp', 0)
+        for h in hosts:
+            for r in rngs:
+                sel = (data['host'] == h) & (data['range'] == r)
+                max_time = data[sel]['time'].max()
+                data_t.loc[sel, 'rt'] = data[sel]['time'] / max_time
+
+        for r in rngs:
+            with open(Path(OUT_DIR) / f'gt0_middle_{r}.dat', 'w') as d:
+                d.write(f"# {r}\n\n")
+                for s in steps:
+                    d.write(f'{s}\t')
+                    for m in METHODS:
+                        sel = (data_t['range'] == r) & (
+                            data_t['step'] == s) & (data_t['method'] == m)
+                        value = data_t[sel]['rt'].sum()
+                        count = data_t[sel]['rt'].count()
+                        d.write(f'\t{value/count} ')
+                    d.write('\n')
+                d.write('\n\n')
+
+
+
+
+
+
+
+
         for rng in rngs:
             c_rng = data[data['range'] == rng]
             sel = c_rng[c_rng['method'] == REF_METHOD]
@@ -92,56 +123,76 @@ def gen_gp_dat(data, graf_type=0):
                 print(stp, end=' ')
                 # for m in METHODS:
                 for hst in hosts:
-                    rng_s = data[data['host'] == hst][data['range'] == rng].sort_values('step')
+                    rng_s = data[data['host'] == hst][data['range']
+                                                      == rng].sort_values('step')
                     steps = rng_s['step'].unique()
                     m2_t = rng_s[rng_s['method'] == 'M2']['time'].to_numpy()
                     m4_t = rng_s[rng_s['method'] == 'M4']['time'].to_numpy()
                     m6_t = rng_s[rng_s['method'] == 'M6']['time'].to_numpy()
                     cf4_t = rng_s[rng_s['method'] == 'CF4']['time'].to_numpy()
-                    cf43_t = rng_s[rng_s['method'] == 'CF4:3']['time'].to_numpy()
+                    cf43_t = rng_s[rng_s['method'] ==
+                                   'CF4:3']['time'].to_numpy()
 
-                
-                    #gp_dat.write("# STEP\tEXECUTION TIME\n")
-                    #gp_dat.write("#       M2\tM4\tM6\tCF4\tCF4:3\n")
+                    # gp_dat.write("# STEP\tEXECUTION TIME\n")
+                    # gp_dat.write("#       M2\tM4\tM6\tCF4\tCF4:3\n")
                     for k, step in enumerate(steps):
                         print(f"\t{m2_t[k]}\t{m4_t[k]}\t{m6_t[k]}" +
-                                    f"\t{cf4_t[k]}\t{cf43_t[k]}\n")
+                              f"\t{cf4_t[k]}\t{cf43_t[k]}\n")
                 print()
             print('\n\n\n')
-
 
     # x = step; y = time
     if graf_type == 2:
+
+        steps = pd.unique(data['step'])
+        hosts = pd.unique(data['host'])
+        data_t = data.copy()
+        data_t.insert(8, 'rt', 0)
+        for h in hosts:
+            for r in rngs:
+                sel = (data['host'] == h) & (data['range'] == r)
+                max_time = data[sel]['time'].max()
+                data_t.loc[sel, 'rt'] = data[sel]['time'] / max_time
+
+        for r in rngs:
+            with open(Path(OUT_DIR) / f'gt2_middle_{r}.dat', 'w') as d:
+                d.write(f"# {r}\n\n")
+                for s in steps:
+                    d.write(f'{s}\t')
+                    for m in METHODS:
+                        sel = (data_t['range'] == r) & (
+                            data_t['step'] == s) & (data_t['method'] == m)
+                        value = data_t[sel]['rt'].sum()
+                        count = data_t[sel]['rt'].count()
+                        d.write(f'\t{value/count} ')
+                    d.write('\n')
+                d.write('\n\n')
+
         for rng in rngs:
-            steps = pd.unique(data[data['range'] == rng]['step'])
-            for stp in steps:
-                print(stp, end=' ')
-                # for m in METHODS:
-                for hst in hosts:
-                    rng_s = data[data['host'] == hst][data['range'] == rng].sort_values('step')
-                    max_time = rng_s['time'].max()
+            with open(Path(OUT_DIR) / f'gt2_collected_{rng}.dat', 'w') as d:
+                d.write(f'{rng}\n\n')
+                steps = pd.unique(data_t[data_t['range'] == rng]['step'])
+                for stp in steps:
+                    d.write(f'{stp}\t')
+                    # for m in METHODS:
+                    for hst in hosts:
+                        sel = (data_t['host'] == hst) & (data_t['range'] == rng)
+                        rng_s = data_t[sel]
+                        rng_s = rng_s[rng_s['step'] == stp]
 
-                    steps = rng_s['step'].unique()
-                    m2_t = rng_s[rng_s['method'] == 'M2']['time'].to_numpy() \
-                        / max_time
-                    m4_t = rng_s[rng_s['method'] == 'M4']['time'].to_numpy() \
-                        / max_time
-                    m6_t = rng_s[rng_s['method'] == 'M6']['time'].to_numpy() \
-                        / max_time
-                    cf4_t = rng_s[rng_s['method'] == 'CF4']['time'].to_numpy() \
-                        / max_time
-                    cf43_t = rng_s[rng_s['method'] == 'CF4:3']['time'].to_numpy() \
-                        / max_time
-
-                    # print("## STEP\tRELATIVE EXECUTION TIME\n")
-                    # print("#       M2\tM4\tM6\tCF4\tCF4:3\n")
-                    for k, step in enumerate(steps):
-                        print(f"\t{m2_t[m2_t['step'] = stp]}\t{m4_t[k]}\t{m6_t[k]}" +
-                                f"\t{cf4_t[k]}\t{cf43_t[k]}", end=' ')
-                print()
-            print('\n\n\n')
-    
-
+                        m2_t = rng_s[rng_s['method'] ==
+                                     'M2']['rt'].to_numpy()
+                        m4_t = rng_s[rng_s['method'] ==
+                                     'M4']['rt'].to_numpy()
+                        m6_t = rng_s[rng_s['method'] ==
+                                     'M6']['rt'].to_numpy()
+                        cf4_t = rng_s[rng_s['method'] ==
+                                      'CF4']['rt'].to_numpy()
+                        cf43_t = rng_s[rng_s['method'] ==
+                                       'CF4:3']['rt'].to_numpy()
+                        d.write(f"\t{m2_t[0]}\t{m4_t[0]}\t{m6_t[0]}" +
+                                f"\t{cf4_t[0]}\t{cf43_t[0]}\t")
+                    d.write('\n')
 
     with open(Path(OUT_DIR) / 'collected_data.csv', 'w') as d:
         d.write(data.to_csv())
@@ -165,5 +216,5 @@ if __name__ == '__main__':
             raise
 
     data = collect_data()
-    print(data)
+    # print(data)
     gen_gp_dat(data, 2)
